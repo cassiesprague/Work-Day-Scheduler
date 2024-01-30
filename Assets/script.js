36e1d6a4b2fef843dc6414e748865a83a76ad168
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-01-30 13:35:09 -0500

    added local storage, event listener, and day and time

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,30 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(document).ready(function () {
+  $("#currentDay").text(moment().format("MMMM DD YYYY, h:mm:ss a"));
+  //Above displays the current date and time
+  $(".saveBtn").on("click", function () {
+  //Above is the event listener for the save button
+    console.log(this);
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time,text);
+  //Above puts the items in local storage and shows values
+  })
+
+  $("#hour-9 .description").val(localStorage.getItem("hour9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour17"));
+  //Above will load the saved data from local storage for every hour 9-5
+
+  
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
